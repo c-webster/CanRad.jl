138b4d2651b3607d584fd7881b460e1fd6dc1dc6
Clare Webster <clare.s.webster@gmail.com>
2019-12-17 12:49:51 +0100

    create_tiles function added

--- src/Prepatory_Functions.jl
+++ src/Prepatory_Functions.jl
@@ -14,3 +14,52 @@ function clipdat(pc,pts,peri)
     pcn = pc[kpidx.==1,:]
     return pcn
 end
+
+function create_tiles(basefolder,ptsf)
+
+    tsize = readdlm(basefolder*"/TileSize.txt")
+
+    segs = readdir(basefolder*"/Segments")
+
+    if !ispath(basefolder*"/Tiles")
+        mkdir(basefolder*"/Tiles")
+    end
+
+    pts_all = Int.(readdlm(ptsf))
+
+    tdx = 0
+
+    ptsfname     = String[]
+    inputsegname = String[]
+
+    for sdx in eachindex(segs)
+
+        limits = readdlm(basefolder*"/Segments"*"/"*segs[sdx]*"/"*segs[sdx]*"_analysisarea.txt")
+
+        limx = Int.(collect(limits[1]:tsize[1]:limits[2]))
+        limy = Int.(collect(limits[3]:tsize[1]:limits[4]))
+
+        for x in eachindex(limx[1:end-1]), y in eachindex(limy[1:end-1])
+            # create the tasks
+            idx = (limx[x] .<= pts_all[:,1] .< limx[x+1]) .&
+                    (limy[y] .<= pts_all[:,2] .< limy[y+1])
+
+            if sum(idx) > 0
+                tdx += 1
+                push!(ptsfname,sprintf1.("%03.$(0)f", tdx)*".txt")
+                # writedlm(basefolder*"/Tiles"*"/"*ptsfname[end],Int.(pts_all[idx,:]))
+
+                push!(inputsegname,segs[sdx])
+            end
+        end
+
+    end
+
+    exdir = basefolder*"/Output/"
+    if !ispath(exdir)
+        mkdir(exdir)
+    end
+
+    return ptsfname, inputsegname, exdir
+
+end
